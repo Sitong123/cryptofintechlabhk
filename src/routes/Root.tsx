dcac5e05e3bbc8945a2399aef09608f5a5dd181a
Dean <afei.wuhao@gmail.com>
2025-01-14 10:32:21 +0800

    wip: add modal

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,43 @@
 import Header from '@/components/header';
 import { Outlet } from 'react-router-dom';
+import Modal from '@mui/material/Modal';
+import { useState } from 'react';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function Root() {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Header />
+
       <Outlet />
+
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </>
   );
 }
